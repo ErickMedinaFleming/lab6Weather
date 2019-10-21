ae65c37ffb765bec83355ff7cf432e54d12bfd45
abqu <erick_abqu@hotmail.com>
2019-10-21 04:03:51 -0500

    it works!
    it works

--- app.js
+++ app.js
@@ -5,7 +5,37 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-clima.geocode('Saltillo', function(error,data){
+app.get('/weather', function(req,res){
+	if(!req.query.search){
+		res.send({
+			error: 'Busqueda con search'
+		})
+	}
+	clima.geocode(req.query.search, function(error,response){
+		if(error){
+			return res.send({
+				error: error
+			})
+		}
+		var city = req.query.search
+		clima.tiempo(response.coord[1], response.coord[0], function(error,response){
+			if(error){
+				return res.send({
+					error: error
+				})
+			}
+			res.send({
+				ciudad: city,
+				summary: response.summary,
+				temperatura: response.temp,
+				humedad: response.humidity
+			})
+		})
+
+	})
+})
+
+/*clima.geocode('Saltillo', function(error,data){
 	if(error){
 		console.log(error)
 	}else{
@@ -18,4 +48,14 @@ clima.geocode('Saltillo', function(error,data){
 			}
 		})
 	}
+})*/
+
+app.listen(port,function(req,res){
+	console.log('Working')
+})
+
+app.get('*',function(req,res){
+	res.send({
+		error: 'Ruta no valida'
+	})
 })
